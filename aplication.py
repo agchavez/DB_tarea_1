d92adfc947ab97467d3f79dcdd57edbc6323a052
Angel Chavez <agchavez@unah.hn>
2021-01-31 02:05:40 -0600

    Funcionalidad de la aplicacion competa

--- aplication.py
+++ aplication.py
@@ -10,13 +10,13 @@ class Data:
     def setBrand(self, data):
         self.cursor.execute('''
                 INSERT INTO 
-                    Screen(id_brand, model_name, resolution, type_tech, dimension) VALUES
-                (%d,'%s','%s','%s',%d)
-        ''' % (int(data['id_brand']),
-                data['model_name'],
-                data['resolution'],
-                data['resolution'],
-                int(data['dimension']),
+                    Brand(nameM,locationM,link_page,email,tel_number) VALUES
+                ('%s','%s','%s','%s',%d)
+        ''' % (data['nameM'],
+                data['locationM'],
+                data['link_page'],
+                data['email'],
+                int(data['tel_number']),
                 ))
         self.data.commit()
         print('---- Marca ingresada con exito ---')
@@ -118,6 +118,110 @@ class Data:
             print(row)
         Menu().Menu()
 
+    def updateComputer(self, data):
+        print('-----------ACTUALIZAR COMPUTADORA---------------')
+        self.cursor.execute('''
+                UPDATE 
+                Computer SET
+                id_brand = %d , id_ram = %d, id_storage = %d, id_screen = %d, model_name = '%s', price = %d, link_manual = '%s', quantity = %d, keyboard_lang = '%s', launch_year = %d
+                WHERE Computer.id = %d
+        ''' % (int(data['id_brand']),
+                int(data['id_ram']),
+                int(data['id_storage']),
+                int(data['id_screen']),
+                data['model_name'],
+                int(data['price']),
+                data['link_manual'],
+                int(data['quantity']),
+                data['keyboard_lang'],
+                int(data['launch_year']),
+                int(data['id'])
+                ))
+        self.data.commit()
+        print('------Computadora Actualizada con exito---------')
+        Menu().Menu()  
+
+    def updateRAM(self, data):
+        print('---------------ACTUALIZAR RAM------------------')
+        self.cursor.execute(
+        '''UPDATE  
+            RAM SET
+                id_brand = %d, model_name = '%s', frequency = %d, capacity = %d, type_tech = '%s' 
+            WHERE
+                RAM.id = %d ''' 
+                    % (int(data['id_brand']),data['model_name'],int(data['frequency']),int(data['capacity']),data['type_tech'],int(data['id'])))
+        self.data.commit()
+        print('-----------RAM Actualizada con exito-----------')
+        Menu().Menu()   
+
+    def updateScreen(self, data):
+        print('---------------ACTUALIZAR PANTALLA------------------')
+        self.cursor.execute('''
+                UPDATE 
+                    Screen SET 
+                        id_brand = %d, model_name = '%s', resolution = '%s', type_tech = '%s', dimension = %d 
+                    WHERE 
+                        id = %d
+        ''' % (int(data['id_brand']),
+                data['model_name'],
+                data['resolution'],
+                data['resolution'],
+                int(data['dimension']),
+                int(data['id'])
+                ))
+        self.data.commit()
+        print('-----------Pantalla Actualizada con exito-----------')
+        Menu().Menu() 
+        
+    def updateBrand(self, data):
+        print('---------------ACTUALIZAR MARCA------------------')
+        self.cursor.execute('''
+                UPDATE  
+                    Brand SET
+                    nameM='%s',locationM='%s',link_page='%s',email='%s',tel_number = %d
+                WHERE
+                    id = %d
+        ''' % (data['nameM'],
+                data['locationM'],
+                data['link_page'],
+                data['email'],
+                int(data['tel_number']),
+                int(data['id'])
+                ))
+        self.data.commit()
+        print('-----------Marca Actualizada con exito-----------')
+        Menu().Menu()                     
+
+    def deleteComputer(self, id): 
+        print('---------------ELIMINAR COMPUTADORA------------------')
+        self.cursor.execute("DELETE FROM Computer WHERE id = %d"%int(id))
+        print('-----------Computadora eliminada con exito-----------')
+        self.data.commit()
+        Menu().Menu()
+
+    def deleteRam(self, id): 
+        print('---------------ELIMINAR RAM------------------')
+        self.cursor.execute("DELETE FROM RAM WHERE id = %d"%int(id))
+        print('-----------RAM eliminada con exito-----------')
+        self.data.commit()
+        Menu().Menu()
+
+    def deleteScreen(self, id): 
+        print('---------------ELIMINAR PANTALLA------------------')
+        self.cursor.execute("DELETE FROM Screen WHERE id = %d"%int(id))
+        print('-----------Pantalla eliminada con exito-----------')
+        self.data.commit()
+        Menu().Menu()
+
+    def deleteBrand(self, id): 
+        print('---------------ELIMINAR MARCA------------------')
+        self.cursor.execute("DELETE FROM Brand WHERE id = %d"%int(id))
+        print('-----------Marca eliminada con exito-----------')
+        self.data.commit()
+        Menu().Menu()            
+
+
+
 class Menu:
     def __init__(self):
         self.db = Data()
@@ -137,10 +241,9 @@ class Menu:
         elif seleccion == '2':
             self.insertsMenu()
         elif seleccion == '3':
-            pass
+            self.updateMenu()
         elif seleccion == '4':
-            pass
-            quit()
+            self.DeleteMenu()
         elif seleccion == '5':
             print('Gracias por preferirnos')
             quit()
@@ -159,16 +262,16 @@ class Menu:
         print('Ingrese su opcion: ')
         seleccion = input()
         if  seleccion == '1':
-            data = {"id_brand":"1",
-                    "id_ram":"1",
-                    "id_storage":"1",
-                    "id_screen":"1",
-                    "model_name":"hjghjg",
-                    "price":"1500",
-                    "link_manual":"dasdsad",
-                    "quantity":"15",
-                    "keyboard_lang":"es",
-                    "launch_year":"2020"
+            data = {"id_brand":"",
+                    "id_ram":"",
+                    "id_storage":"",
+                    "id_screen":"",
+                    "model_name":"",
+                    "price":"",
+                    "link_manual":"",
+                    "quantity":"",
+                    "keyboard_lang":"s",
+                    "launch_year":""
                     }
             
             self.db.setComputer(self.enterokayData(data))
@@ -234,6 +337,94 @@ class Menu:
         else:
             print('Opcion no valida')
             self.menuShow
+
+    def updateMenu(self):
+        print('\n\n\n\n============================')
+        print('Menu de Actualizaciones de datos:')
+        print('1. Actualizar computadoras')
+        print('2. Actualizar RAM')
+        print('3. Actualizar pantallas')
+        print('4. Actualizar marcas')
+        print('5. regresar al menu')
+        print('Ingrese su opcion: ')
+        seleccion = input()   
+        print('Ingrese el id del campo a editar: ')
+        if  seleccion == '1':
+            data = {"id":"",
+                    "id_brand":"",
+                    "id_ram":"",
+                    "id_storage":"",
+                    "id_screen":"",
+                    "model_name":"",
+                    "price":"",
+                    "link_manual":"",
+                    "quantity":"",
+                    "keyboard_lang":"",
+                    "launch_year":""
+                    }
+            
+            self.db.updateComputer(self.enterokayData(data))
+        elif seleccion == '2':
+            data = {"id":"",
+                    "id_brand":"",
+                    "model_name":"",
+                    "frequency":"",
+                    "capacity":"",
+                    "type_tech":""
+                    }
+            self.db.updateRAM(self.enterokayData(data))
+        elif seleccion == '3':
+            data = {"id":"",
+                    "id_brand":"",
+                    "model_name":"",
+                    "resolution":"",
+                    "type_tech":"",
+                    "dimension":""
+                    }
+            self.db.updateScreen(self.enterokayData(data))
+        elif seleccion == '4':
+            data = {
+                    "nameM":"",
+                    "locationM":"",
+                    "link_page":"",
+                    "email":"",
+                    "tel_number":""
+                    }
+            self.db.updateBrand(self.enterokayData(data))
+        elif seleccion == '5':
+            self.Menu()
+        else:
+            print('Opcion no valida')
+            self.menuShow
+
+    def DeleteMenu(self):
+        print('\n\n\n\n============================')
+        print('Que desea eliminar?:')
+        print('1. Eliminar computadoras')
+        print('2. Eliminar RAM')
+        print('3. Eliminar pantallas')
+        print('4. Eliminar marcas')
+        print('5. regresar al menu')
+        print('Ingrese su opcion: ')
+        seleccion = input()  
+        print("Ingrese el id a eliminar: ")
+        id = input() 
+        if  seleccion == '1':
+            self.db.deleteComputer(id)
+        elif seleccion == '2':                    
+                    
+            self.db.deleteRam(id)
+        elif seleccion == '3':                    
+                    
+            self.db.deleteScreen(id)
+        elif seleccion == '4':                    
+                    
+            self.db.deleteBrand(id)
+        elif seleccion == '5':
+            self.Menu()
+        else:
+            print('Opcion no valida')
+            self.menuShow                 
         
 
     
